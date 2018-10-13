ddac3c6ef5f19fe6171ce4a27191cf10d952825f
dylan_DPC <dylan.dpc@gmail.com>
2018-10-14 01:49:59 +0530

    other options to builder.. still WIP

--- src/builder.rs
+++ src/builder.rs
@@ -1,5 +1,6 @@
 use super::{Clim, MenuOption };
 use std::fmt::Display;
+use failure::Error;
 
 pub struct Builder<T>
 where
@@ -25,6 +26,59 @@ impl<T> Builder<T>
 
 }
 
+pub struct OptionBuilder<T>
+where
+    T: Display + Eq + Into<String> + Clone + Default
+{
+    key: T,
+    description: String,
+    on_select: Box<Fn()>,
+    is_exit: bool
+}
+
+impl<T> OptionBuilder<T>
+where
+    T: Display + Eq + Into<String> + Clone + Default
+{
+    pub fn new() -> OptionBuilder<T> {
+        OptionBuilder {
+            key: T::default(),
+            description: String::from(""),
+            on_select: Box::new(|| {}),
+            is_exit: false,
+        }
+    }
+
+    pub fn build(self) ->  MenuOption<T> {
+        MenuOption::new(self.key, self.description, self.on_select, self.is_exit)
+    }
+
+    pub fn key<U: Into<T>>(&mut self, key: U) -> &mut Self {
+        self.key = key.into().clone();
+
+        self
+    }
+    pub fn description<U: Into<String>>(&mut self, description: U) -> &mut Self {
+        self.description = description.into();
+
+        self
+    }
+    pub fn on_select<F>(&mut self, on_select: F) -> &mut Self
+    where F: Fn() -> () + 'static
+    {
+        self.on_select = Box::new(on_select);
+
+        self
+    }
+    pub fn exits(&mut self) -> &mut Self {
+        self.is_exit = true;
+
+        self
+    }
+
+
+}
+
 #[cfg(test)]
 mod tests{
     use super::*;
@@ -39,9 +93,12 @@ mod tests{
             }),
             is_exit: false,
         };
-        let mut builder = Builder::new("builder test");
+        let mut builder: Builder<String> = Builder::new("builder test");
 
-        builder.add(option);
+        builder.add(OptionBuilder::new().key("1").description("foo bar baz").on_select(|| {
+            println!("yeeeee");
+        })
+            .build());
 
         assert_eq!(builder.title, "builder test");
         assert_eq!(builder.options[0].key, "1".to_string());
